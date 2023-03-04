671a0e5d11ac42317f005f469379a70276fd1494
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-03 21:00:11 -0800

    updated function to arrow function, added error handling

--- step2.js
+++ step2.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
 const axios = require('axios');
 
-function cat(path) {
+const cat = (path) => {
     fs.readFile(path, 'utf-8', (err, data) => {
         if (err) process.kill(1);
         console.log(data);
     })
 }
 
-cat('./one.txt')
+const webCat = (url) => {
+    axios.get(url)
+    .then(data => console.log(data))
+    .catch(err => console.log('Oops forgot to add another arg \n Try ==> "node step2.js <path>"'));
+}
+
+(process.argv[2]==='one.txt')?cat(process.argv[2]):webCat(process.argv[2]);
