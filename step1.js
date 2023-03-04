ac6a9f054957827853202b056afb77dd8e0d9dac
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-03 20:36:38 -0800

    added error handling by killing process when there is an invalid path

--- step1.js
+++ step1.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 
 function cat(path) {
     fs.readFile(path, 'utf-8', (err, data) => {
-        console.log(data)
+        if (err) process.kill(1);
+        console.log(data);
     })
 }
 
