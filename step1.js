431aff7efa210e72a14fc0ddec34f420a1abd820
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-03 20:34:57 -0800

    added encoding set to utf-8

--- step1.js
+++ step1.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 
 function cat(path) {
-    fs.readFile(path, (err, data) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
         console.log(data)
     })
 }
+
+cat('./one.txt')
